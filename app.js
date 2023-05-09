c27a1f191d43103089179af5cb8042f8338644fd
sang ah <kr.sangah@gmail.com>
2023-05-09 15:49:43 +0900

    express 배포 코드 추가

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
-app.set("/", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 배포 서버 실행");
 });
